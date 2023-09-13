9ba26a163287489b27779a079801b6c187d99980
Scott Nelson <snelson@instructors.2u.com>
2023-09-13 13:04:22 -0700

    finished remove and edit feature

--- app.js
+++ app.js
@@ -7,25 +7,28 @@ const rl = readline.createInterface({
   output: process.stdout, // Write to standard output (console)
 });
 
-
 function mainMenu() {
   rl.question(
     `What would you like to do? (enter action number or press Ctrl+C to quit)\n
     1) View grocery list\n
     2) Add new item to grocery list\n
     3) Remove item from grocery list\n
-    4) Change item status to bought\n`,
+    4) Set item's bought status\n`,
     (answer) => {
       switch (answer.trim()) {
         case "1":
-            viewGroceryList();
-            mainMenu();
+          viewGroceryList();
+          mainMenu();
           break;
         case "2":
-            addNewGroceryItem();
+          addNewGroceryItem();
+          break;
+        case "3":
+          removeGroceryItem();
+          break;
+        case "4":
+            changeGroceryItemStatus();
             break;
-            case "3":
-                break;
         default:
           console.log("Please enter a valid action number");
           break;
@@ -37,34 +40,68 @@ function mainMenu() {
     // end of input
   });
 }
+
 function viewGroceryList() {
-    printGroceryList();
+  if (groceryItems.length < 1) {
+    console.log("There are no grocery items in the list yet.\n");
     mainMenu();
+    return;
+  }
+
+  printGroceryList();
+  mainMenu();
 }
 
 function printGroceryList() {
-    console.log(`\tNumber\t\t\tName\t\t\tQuantity\t\t\tPrice\t\t\tBought\n`);
-    groceryItems.forEach((item, index) => {
-       console.log(`\t${index + 1}\t\t\t${item.name}\t\t\t${item.qty}\t\t\t\t$${item.price}\t\t\t${item.bought ? "Yes": "No"}`);
-    });
+console.table(groceryItems);
 }
 
 function addNewGroceryItem() {
-    const newGroceryItem = {bought: false};
-    rl.question("What is the name of the item you wish to add?\n", (name) => {
-        newGroceryItem.name = name.trim();
-        rl.question("Enter the quantity needed\n", (qty) => {
-            newGroceryItem.qty = parseInt(qty.trim());
-            rl.question("Enter the item price\n", (price) => {
-                newGroceryItem.price = parseFloat(price.trim());
-                groceryItems.push(newGroceryItem);
-                mainMenu();
-            })
-        });    
+  const newGroceryItem = { bought: false };
+  rl.question("What is the name of the item you wish to add?\n", (name) => {
+    newGroceryItem.name = name.trim();
+    rl.question("Enter the quantity needed\n", (qty) => {
+      newGroceryItem.qty = parseInt(qty.trim());
+      rl.question("Enter the item price\n", (price) => {
+        newGroceryItem.price = parseFloat(price.trim());
+        groceryItems.push(newGroceryItem);
+        mainMenu();
+      });
     });
+  });
 }
 
+function removeGroceryItem() {
+  if (groceryItems.length < 1) {
+    console.log("There are no grocery items in the list yet.\n");
+    mainMenu();
+    return;
+  }
 
+  printGroceryList();
+  rl.question("Enter the index number of the item you wish to remove\n", (answer) => {
+    const index = parseInt(answer.trim());
+    groceryItems.splice(index, 1);
+    mainMenu();
+  });
+}
 
+function changeGroceryItemStatus() {
+  if (groceryItems.length < 1) {
+    console.log("There are no grocery items in the list yet.\n");
+    mainMenu();
+    return;
+  }
+
+  printGroceryList();
+  rl.question(
+    "Enter the index number of the item that you wish to change the bought status for\n",
+    (answer) => {
+      const index = parseInt(answer.trim());
+      groceryItems[index].bought = !groceryItems[index].bought;
+      mainMenu();
+    }
+  );
+}
 
 mainMenu();
